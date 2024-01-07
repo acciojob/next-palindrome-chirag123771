ee57b6fc5655fb5df09e8a3e632316f0df46cde9
Chirag Agarwal <72219269+chirag123771@users.noreply.github.com>
2024-01-08 00:12:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function nextPalindrome(num) {
   //your JS code here.
+	 if (typeof num !== 'number' || num <= 0 || num > 100000 || !Number.isInteger(num)) {
+        alert("Please enter a valid positive integer up to 100000.");
+        return;
+    }
+
+    // Function to check if a number is a palindrome
+    function isPalindrome(num) {
+        const numStr = num.toString();
+        return numStr === numStr.split('').reverse().join('');
+    }
+
+    // Find the next palindrome
+    let nextNumber = num + 1;
+    while (!isPalindrome(nextNumber)) {
+        nextNumber++;
+    }
+
+    // Display the result
+    alert("The next palindrome after " + num + " is: " + nextNumber);
 }
 
 const input = prompt("Enter a palindrome number");
